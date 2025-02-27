9f6cd3ebd213e884e4b682f291a93f41821a4a5f
abdullah-albanna <abdu.albanna@proton.me>
2025-02-27 20:59:13 +0300

    better device checking

--- backend/src/device_management/device.rs
+++ backend/src/device_management/device.rs
@@ -101,62 +101,121 @@ pub fn check_installing_succeed(window: tauri::Window) {
     }
 }
 
+/// represents the device's connection state
+#[derive(Debug)]
+enum DeviceState {
+    Connected,
+    Disconnected,
+}
+
 #[tauri::command]
 pub fn check_device(window: tauri::Window) {
-    let device_disconnected = Arc::new(Mutex::new(false));
-
-    // we start out with true to send the device info if it's connnected
-    //
-    // if it was false then we will only send the device info if it got disconnected then
-    // reconnected, we don't want that
-    let status_changed = Arc::new(Mutex::new(true));
+    let current_state = Arc::new(Mutex::new(DeviceState::Disconnected));
 
     thread::spawn({
         move || loop {
-            if let Ok(device) = rsmobiledevice::device::DeviceClient::new() {
-                let Some(device) = device.get_first_device() else {
-                    continue;
-                };
+            let device_client = rsmobiledevice::device::DeviceClient::new()
+                .map(rsmobiledevice::device::DeviceClient::get_first_device);
 
-                if let (Ok(mut status_c), Ok(mut device_disconnected_value)) =
-                    (status_changed.try_lock(), device_disconnected.try_lock())
-                {
-                    // only if the status changed we send it again
-                    if *status_c {
-                        let hardware = handle_device_hardware(&device);
-                        let storage = handle_device_storage(&device);
-                        let battery = handle_device_battery(&device);
-                        let os = handle_device_os(&device);
+            let device_connected = device_client.as_ref().is_ok_and(|d| d.as_ref().is_some());
 
-                        // Emit connected status with device info
-                        window.emit("device_hardware", hardware).unwrap();
-                        window.emit("device_storage", storage).unwrap();
-                        window.emit("device_battery", battery).unwrap();
-                        window.emit("device_os", os).unwrap();
+            let mut state = current_state.lock().expect("This should be okay");
 
-                        // we set the disconnected value to false indicating it is connnected
-                        *device_disconnected_value = false;
-                        *status_c = false;
-                        window.emit("device_status", true).unwrap();
-                    }
+            match *state {
+                DeviceState::Disconnected if device_connected => {
+                    window
+                        .emit("device_status", true)
+                        .expect("Failed to emit `device_status`");
+
+                    let Ok(Some(device)) = device_client else {
+                        continue;
+                    };
+
+                    window
+                        .emit("device_hardware", handle_device_hardware(&device))
+                        .unwrap();
+                    window
+                        .emit("device_storage", handle_device_storage(&device))
+                        .unwrap();
+                    window
+                        .emit("device_battery", handle_device_battery(&device))
+                        .unwrap();
+                    window.emit("device_os", handle_device_os(&device)).unwrap();
+
+                    *state = DeviceState::Connected;
                 }
-            } else if let (Ok(mut device_disconnected_value), Ok(mut status_changed_value)) =
-                (device_disconnected.try_lock(), status_changed.try_lock())
-            {
-                // if the value is false, meaning it was connected, and now it got disconnected
-                if !(*device_disconnected_value) {
+                DeviceState::Connected if !device_connected => {
                     window.emit("device_status", false).unwrap();
-                    // we set the disconnected value to true indicating that it is disconnected
-                    //
-                    // also we put the status changed to true, because it got disconnected
-                    // even if it was disconnected from the beginning, we will only assign it back
-                    // to true once
-                    *device_disconnected_value = true;
-                    *status_changed_value = true;
+                    *state = DeviceState::Disconnected;
                 }
+                _ => {}
             }
 
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
+
+//#[tauri::command]
+//pub fn check_device_(window: tauri::Window) {
+//    let device_disconnected = Arc::new(Mutex::new(false));
+//
+//    // we start out with true to send the device info if it's connnected
+//    //
+//    // if it was false then we will only send the device info if it got disconnected then
+//    // reconnected, we don't want that
+//    let status_changed = Arc::new(Mutex::new(true));
+//
+//    thread::spawn({
+//        move || loop {
+//            let device_connected = rsmobiledevice::device::DeviceClient::new()
+//                .is_ok_and(|d| d.get_first_device().is_some());
+//
+//
+//            if let Ok(device) = rsmobiledevice::device::DeviceClient::new() {
+//                let Some(device) = device.get_first_device() else {
+//                    continue;
+//                };
+//
+//                if let (Ok(mut status_c), Ok(mut device_disconnected_value)) =
+//                    (status_changed.try_lock(), device_disconnected.try_lock())
+//                {
+//                    // only if the status changed we send it again
+//                    if *status_c {
+//                        let hardware = handle_device_hardware(&device);
+//                        let storage = handle_device_storage(&device);
+//                        let battery = handle_device_battery(&device);
+//                        let os = handle_device_os(&device);
+//
+//                        // Emit connected status with device info
+//                        window.emit("device_hardware", hardware).unwrap();
+//                        window.emit("device_storage", storage).unwrap();
+//                        window.emit("device_battery", battery).unwrap();
+//                        window.emit("device_os", os).unwrap();
+//
+//                        // we set the disconnected value to false indicating it is connnected
+//                        *device_disconnected_value = false;
+//                        *status_c = false;
+//                        window.emit("device_status", true).unwrap();
+//                    }
+//                }
+//            } else if let (Ok(mut device_disconnected_value), Ok(mut status_changed_value)) =
+//                (device_disconnected.try_lock(), status_changed.try_lock())
+//            {
+//                // if the value is false, meaning it was connected, and now it got disconnected
+//                if !(*device_disconnected_value) {
+//                    window.emit("device_status", false).unwrap();
+//                    // we set the disconnected value to true indicating that it is disconnected
+//                    //
+//                    // also we put the status changed to true, because it got disconnected
+//                    // even if it was disconnected from the beginning, we will only assign it back
+//                    // to true once
+//                    *device_disconnected_value = true;
+//                    *status_changed_value = true;
+//                }
+//            }
+//
+//            thread::sleep(Duration::from_secs(1));
+//        }
+//    });
+//}
